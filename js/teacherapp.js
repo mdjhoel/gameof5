2913c5d286e2eebe6dca21753442d1f2984a1c16
Mark Hoel <mdjhoel@gmail.com>
2018-08-23 15:22:58 -0400

    added app.filter unique
    called from admin_users and admin_list

--- js/teacherapp.js
+++ js/teacherapp.js
@@ -1,5 +1,32 @@
 var app = angular.module('teacherpages', ['ngRoute','ngSanitize']);
-    
+   
+  app.filter('unique', function() {
+    // we will return a function which will take in a collection
+    // and a keyname
+    return function(collection, keyname) {
+      // we define our output and keys array;
+      var output = [], 
+          keys = [];
+      
+      // we utilize angular's foreach function
+      // this takes in our original collection and an iterator function
+      angular.forEach(collection, function(item) {
+          // we check to see whether our object exists
+          var key = item[keyname];
+          // if it's not already part of our keys array
+          if(keys.indexOf(key) === -1) {
+              // add it to our keys array
+              keys.push(key); 
+              // push this item to our final output array
+              output.push(item);
+          }
+      });
+      // return our array which should be devoid of
+      // any duplicates
+      return output;
+   };
+  });
+
   app.run(function($rootScope,$location) {
 
     // get url so URL can be put back when users click on modals
